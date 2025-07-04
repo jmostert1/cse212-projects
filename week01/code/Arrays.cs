ea6bf79800e13179d184d587de07cdb61bc6d124
jmostert1 <joshuamostert9@gmail.com>
2025-07-04 11:57:04 +0200

    Weekly code activity - Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 public static class Arrays
 {
     /// <summary>
@@ -13,7 +15,15 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        //create new array with length = length
+        double[] multi = new double[length];
+        //loop and give each index a corresponding multiple
+        for (int i = 0; i < length; i++)
+        {
+            multi[i] = number * (i+1);
+        }
+
+        return multi; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +39,21 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        //using GetRange method
+        //get num elements in list
+        int count = data.Count;
+
+        amount = amount % count;  // Normalize rotations if amount > count (work the index in the list, cant go over)
+                                  // Get last amount elements
+        var tail = data.GetRange(count - amount, amount);
+        // Remove last amount elements from original list
+        data.RemoveRange(count - amount, amount);
+        // Insert those amount elements at the front
+        data.InsertRange(0, tail);
+        
     }
-}
+
+       
+ }
+
